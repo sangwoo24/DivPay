57c3a6a430a93e50ba84c77d5c8dae225ae1243e
Jerry <jerry.seok@blueant.kr>
2022-07-04 22:27:35 +0900

    feat : AppColor, Size Class 작성

---
+++ divpay/lib/Common/Util/app_color.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const Color background = Color.fromRGBO(135, 86, 228, 1);
+  static const Color secondaryBackground = Color.fromRGBO(109, 90, 207, 1);
+  static const Color point = Color.fromRGBO(255, 196, 102, 1);
+  static const Color content = Color.fromRGBO(255, 255, 255, 1);
+  static const Color darkContent = Color.fromRGBO(80, 76, 107, 1);
+  static const Color darkContentSecondary = Color.fromRGBO(80, 76, 123, 1);
+}
---
+++ divpay/lib/Common/Util/size.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+Widget Space({double width = 0, double height = 0}) {
+  return SizedBox(
+    width: width,
+    height: height,
+  );
+}
